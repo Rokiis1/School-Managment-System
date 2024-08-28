f2e04f3d46c383837d46e5babe065e7cf5622c79
Rokiis1 <dailyuseracc1@gmail.com>
2024-08-28 08:07:03 +0300

    Implemented postgreSQL connection

--- server/server.mjs
+++ server/server.mjs
@@ -1,28 +1,52 @@
 import express from "express";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV === "prod") {
+  dotenv.config({ path: ".env.prod" });
+} else {
+  dotenv.config({ path: ".env.dev" });
+}
 
 import routes from "./routes/index.mjs";
+import { connectDB } from "./db/postgresConnection.mjs";
 
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-app.get("/api/v1/health", (req, res) => {
+const initializeDatabase = async () => {
   try {
-    res.status(200).json({
-      status: "success",
-      message: "Server is healthy",
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: `Internal Server Error, ${error}`,
-    });
+    await connectDB();
+    console.log("Database initialization complete");
+  } catch (err) {
+    console.error("Failed to initialize the database", err);
+    process.exit(1);
   }
-});
+};
 
-app.use("/api/v1", routes);
+const startServer = async () => {
+  await initializeDatabase();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  const app = express();
+  const PORT = process.env.APP_PORT;
+
+  app.use(express.json());
+
+  app.get("/api/v1/health", (req, res) => {
+    try {
+      res.status(200).json({
+        status: "success",
+        message: "Server is healthy",
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: `Internal Server Error, ${error}`,
+      });
+    }
+  });
+
+  app.use("/api/v1", routes);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
